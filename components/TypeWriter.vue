f82434933f723f111d09a5fb91badabe3258d411
Nuno Mendes <nuno.laurentino.mendes@cern.ch>
2024-03-19 23:35:59 +0000

    Fix spaces on Typewriter effect

--- components/TypeWriter.vue
+++ components/TypeWriter.vue
@@ -1,52 +1,52 @@
 <template>
     <div class="text-base sm:text-xl md:text-3xl lg:text-5xl font-bold text-center text-white">
-      <span v-for="(char, index) in wordsArray" :key="index"
-        :style="{ 'animation-delay': `${index * 0.1}s` }"
-        class="character-animation">
-        {{ char }}
-      </span>
-      <span class="cursor-animation"></span>
+        <template v-for="(item, index) in wordsArray" :key="index">
+            <span v-if="item === ' '" class="space" :style="{ 'animation-delay': `${index * 0.1}s` }">&nbsp;</span>
+            <span v-else :style="{ 'animation-delay': `${index * 0.1}s` }" class="character-animation text-white">
+                {{ item }}
+            </span>
+        </template>
+        <span class="cursor-animation bg-blue-500"></span>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const props = defineProps({
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
     words: String,
-  });
-  
-  const wordsArray = ref([...props.words]);
-  </script>
-  
-  <style scoped>
-  .character-animation {
+});
+
+const wordsArray = ref([...props.words]);
+</script>
+
+<style scoped>
+.character-animation {
     display: inline-block;
     opacity: 0;
     transform: translateY(-100%);
     animation: slideIn 0.6s forwards;
-  }
-  
-  @keyframes slideIn {
+}
+
+@keyframes slideIn {
     to {
-      opacity: 1;
-      transform: translateY(0);
+        opacity: 1;
+        transform: translateY(0);
     }
-  }
-  
-  .cursor-animation {
+}
+
+.cursor-animation {
     display: inline-block;
     background-color: blue;
     width: 4px;
     height: 1em;
     margin-left: 5px;
     animation: blink 1s step-start infinite;
-  }
-  
-  @keyframes blink {
+}
+
+@keyframes blink {
     50% {
-      opacity: 0;
+        opacity: 0;
     }
-  }
-  </style>
-  
+}
+</style>
