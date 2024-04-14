3d10e083188e8ee4f7ed2ed20ed17732eab18898
Nuno Mendes <nuno.laurentino.mendes@cern.ch>
2024-04-14 11:45:13 +0200

    Improve homepage achievements

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
 
             <p>🏠 Living in Nyon, Switzerland 🇨🇭</p>
 
-            <p>💼 Currently Senior Full Developer at <span
+            <p>💼 Currently Senior Full Stack Developer at <span
                     class="text-yellow-500 hover:text-yellow-300 transition duration-300 ease-in-out">Breitling</span>
                 ⏱️</p>
 
@@ -45,13 +45,13 @@
 
             <div class="gap-5 flex flex-col">
                 <div
-                    class="bg-gray-300 rounded-md mx-3 p-2 text-center text-black text-opacity-70 hover:text-opacity-100 transition duration-300 ease-in-out transform hover:scale-105">
-                    Creating a full-stack application to monitor and control thousands of Power Converters at CERN,
+                    class="border-2 border-yellow-600 bg-yellow-300 rounded-md mx-3 p-2 text-center text-orange-900 text-opacity-90 hover:text-opacity-100 transition duration-300 ease-in-out transform hover:scale-105">
+                    Created a full-stack application to monitor and control thousands of Power Converters at CERN,
                     leveraging interactive data visualization to enhance operational efficiency and user experience.
                 </div>
                 <div
-                    class="bg-gray-300 rounded-md mx-3 p-2 text-center text-black text-opacity-70 hover:text-opacity-100 transition duration-300 ease-in-out transform hover:scale-105">
-                    Creating a data storage engine for Power Converters at CERN, allowing for real-time data analysis or
+                    class="border-2 border-yellow-600 bg-yellow-300 rounded-md mx-3 p-2 text-center text-orange-900 text-opacity-90 hover:text-opacity-100 transition duration-300 ease-in-out transform hover:scale-105">
+                    Created a data storage engine for Power Converter Acquisition logs at CERN, allowing for real-time data analysis or
                     historical data retrieval.
                 </div>
             </div>
