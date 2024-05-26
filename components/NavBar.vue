e7eb655c7b3b826c4d9be3337c2e93e3a9832634
Nuno Mendes <nuno.laurentino.mendes@cern.ch>
2024-05-26 21:27:40 +0200

    Navbar Links should not open a new tab

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -16,7 +16,7 @@ const sections = [
         <div class="flex flex-row items-center space-x-4 p-4 bg-gray-900 text-white shadow-purple-700 shadow-lg rounded-full">
             <div class="flex flex-row space-x-4">
                 <div v-for="item in sections[0].items" :key="item.link" class="hover:text-purple-300 transition-colors duration-300">
-                    <NuxtLink :to="item.link" :title="item.title" target="_blank" rel="noopener noreferrer" class="flex items-center">
+                    <NuxtLink :to="item.link" :title="item.title" class="flex items-center">
                         <font-awesome-icon :icon="item.icon" class="w-8 h-8" size="lg" />
                         <span class="ml-2 font-medium text-sm">{{ item.title }}</span>
                     </NuxtLink>
